a687ab9f65f5ee90b8b35db6fc84e367616ec4df
Sergey Kirichenko <s2002kir@yandex.ru>
2019-06-26 09:37:45 +0300

    Make keyboard one-time

--- keyboard.py
+++ keyboard.py
@@ -1,5 +1,6 @@
 from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 keyboard = lambda buttons: ReplyKeyboardMarkup(
-        [[KeyboardButton(x) for x in xs] for xs in buttons]
+        [[KeyboardButton(x) for x in xs] for xs in buttons],
+        one_time_keyboard = True
     )
